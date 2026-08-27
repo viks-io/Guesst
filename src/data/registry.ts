f59bbf7276e167f874d538d92cb6bf8df0762f59
viks-io <vikaas65@gmail.com>
2026-08-27 20:34:26 +0100

    Empty initial baseline

--- src/data/registry.ts
+++
@@ -1,9 +0,0 @@
-import type { Category } from "../game/types";
-import { footballPlayersCategory } from "./categories/footballPlayers";
-
-/**
- * Every playable category. Adding a new one later is just:
- * 1. create `data/categories/<name>.ts` exporting a Category
- * 2. add it to this list
- */
-export const CATEGORIES: Category[] = [footballPlayersCategory];
